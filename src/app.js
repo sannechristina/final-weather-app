2c0015b7d375537fe64c97765a8983bf963e8cce
sannechristina <info@sannechristina.com>
2021-03-30 17:35:48 +0200

    Added newDate

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  //calculate the date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data.main);
   let temperatureElement = document.querySelector("#temperature");
@@ -11,7 +35,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].main;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = "today";
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "2982a53a67a2af7d8bfffdecb27b8eb2";
