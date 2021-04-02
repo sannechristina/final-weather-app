617dd577d1586e68de2bf575c5599857b97d04f7
sannechristina <info@sannechristina.com>
2021-04-02 11:07:40 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -45,9 +45,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "2982a53a67a2af7d8bfffdecb27b8eb2";
-let city = "Den Bosch";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+function search(city) {
+  let apiKey = "2982a53a67a2af7d8bfffdecb27b8eb2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Den Bosch");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
